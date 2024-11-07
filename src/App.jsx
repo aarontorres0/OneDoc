87469e0c6ffb3166555a31ca6febf4bcb8239455
Aaron Torres <aarontorres1423@gmail.com>
2024-11-06 21:45:40 -0800

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-5xl font-bold text-gray-800">OneDoc</h1>
+    </div>
+  );
+}
+
+export default App;
